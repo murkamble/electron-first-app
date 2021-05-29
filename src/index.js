ee1945f57bf76bc98d528a689c5e7372fab9c13e
cse-mkamble <76428798+cse-mkamble@users.noreply.github.com>
2021-05-29 11:58:21 +0530

    V1.0.0

--- src/index.js
+++ src/index.js
@@ -11,13 +11,14 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(__dirname, 'facebook.ico')
   });
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
